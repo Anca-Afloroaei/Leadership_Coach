cb4e8b3ea4f55544bb4cdaa6b79bf11403f7fa9b
Anca-Afloroaei <anca.afloroaei@gmail.com>
2025-08-05 15:51:47 +0200

    Refactor Settings class to improve configuration structure

--- backend/config.py
+++ backend/config.py
@@ -2,11 +2,13 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+
     SUPABASE_PASSWORD: str
     DATABASE_URL: str
     SUPABASE_URL: str
+    OPENAI_API_KEY: str
     
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-
+    
 
 settings = Settings()
