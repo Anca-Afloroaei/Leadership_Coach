31d7c01f74df5cc4e25d168563e9957a18164fdd
Anca-Afloroaei <anca.afloroaei@gmail.com>
2025-08-05 17:02:54 +0200

    Created a helper script for making a questionnaire from all available questions

---
+++ backend/helpers/questionnaire_maker.py
@@ -0,0 +1,27 @@
+from features.questions.service import list_questions
+from features.questionnaires.service import create_questionnaire
+from database.core import get_session
+from fastapi import Depends
+from features.questionnaires.models import QuestionnaireCreate
+
+
+
+if __name__ == "__main__":
+
+
+    session = next(get_session())
+    questions_read_list = list_questions(session)
+    print(f"Questions read: {questions_read_list}")
+    questions_list = [question.id for question in questions_read_list]
+    
+    # print(f"Questions list: {questions_list}")
+
+    questionnaire_create = QuestionnaireCreate(
+        title="Sample Questionnaire",
+        description="This is a sample questionnaire created for testing purposes.",
+        questions=questions_list, 
+        )
+    questionnaire = create_questionnaire(questionnaire_create, session=session)
+    print(f"Questionnaire created with ID: {questionnaire.id}")
+
+
