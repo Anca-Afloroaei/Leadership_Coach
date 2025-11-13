e6ce189ec8592d79205bec567e7365a63c3b4805
Anca-Afloroaei <anca.afloroaei@gmail.com>
2025-11-13 17:20:24 +0100

    Refactor: Reorganize import statements for improved clarity in routers.py

--- backend/routers.py
+++ backend/routers.py
@@ -1,15 +1,27 @@
 from fastapi import FastAPI
-from features.users.controller import router as users_router
-from features.leadership_assessments.controller import router as assessments_router
-from features.development_plans.controller import router as development_plans_router
-from features.user_module_progress.controller import router as user_module_progress_router
-from features.leadership_modules.controller import router as leadership_modules_router
-from features.questionnaires.controller import router as questionnaires_router
+
 from features.answers.controller import router as answers_router
-from features.questions.controller import router as questions_router
-from features.question_and_answers.controller import router as question_and_answers_router
 from features.auth.controller import router as auth_router
+from features.development_plans.controller import (
+    router as development_plans_router,
+)
+from features.leadership_assessments.controller import (
+    router as assessments_router,
+)
+from features.leadership_modules.controller import (
+    router as leadership_modules_router,
+)
+from features.question_and_answers.controller import (
+    router as question_and_answers_router,
+)
+from features.questionnaires.controller import router as questionnaires_router
+from features.questions.controller import router as questions_router
+from features.results.controller import router as results_router
 from features.user_answers.controller import router as user_answers_router
+from features.user_module_progress.controller import (
+    router as user_module_progress_router,
+)
+from features.users.controller import router as users_router
 
 
 def register_routers(app: FastAPI):
@@ -28,3 +40,4 @@ def register_routers(app: FastAPI):
     app.include_router(questionnaires_router)
     app.include_router(auth_router)
     app.include_router(user_answers_router)
+    app.include_router(results_router)
